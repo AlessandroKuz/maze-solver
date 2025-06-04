971f158091c3871b635526ccb26ec469d2aba87e
AlessandroKuz <akuzcontact@gmail.com>
2025-06-04 12:49:24 +0200

    feat: add Line and Point classes + code refactor

---
+++ graphics.py
@@ -0,0 +1,42 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, start: Point, end: Point) -> None:
+        self.start = start
+        self.end = end
+
+    def draw(self, canvas: Canvas, fill_color: str) -> None:
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_color, width=2)
+
+
+class Window:
+    def __init__(self, width: int, height: int) -> None:
+        self.__root = Tk()
+        self.__root.title('Maze Solver')
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=True)
+        self.__is_running = False
+
+    def redraw(self) -> None:
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self) -> None:
+        self.__is_running = True
+        while self.__is_running:
+            self.redraw()
+        print("Window closed...")
+
+    def draw_line(self, line: Line, fill_color: str = "black") -> None:
+        line.draw(self.__canvas, fill_color)
+
+    def close(self) -> None:
+        self.__is_running = False
--- main.py
+++ main.py
@@ -1,31 +1,16 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window, Line, Point
 
-class Window:
-    def __init__(self, width, height) -> None:
-        self.__root = Tk()
-        self.__root.title('Maze Solver')
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
-        self.__canvas.pack(fill=BOTH, expand=True)
-        self.__is_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.__is_running = True
-        while self.__is_running:
-            self.redraw()
-        print("Window closed...")
-
-    def close(self):
-        self.__is_running = False
+def main():
+    window = Window(800, 600)
+    window.draw_line(Line(Point(100, 100), Point(200, 200)), "red")
+    window.draw_line(Line(Point(200, 100), Point(100, 200)), "blue")
+    window.draw_line(Line(Point(100, 100), Point(200, 100)), "black")
+    window.draw_line(Line(Point(100, 200), Point(200, 200)), "black")
+    window.draw_line(Line(Point(100, 100), Point(100, 200)), "black")
+    window.draw_line(Line(Point(200, 100), Point(200, 200)), "black")
+    window.wait_for_close()
 
 
 if __name__ == '__main__':
-    # win = Window(800, 600)
-    # win.wait_for_close()
-    window = Window(800, 600)
-    window._Window__canvas.create_rectangle(50, 50, 150, 150, fill="blue")
-    window.wait_for_close()
+    main()
