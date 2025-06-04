822a4b03606dad4773b8aaac31f269c6ce863059
AlessandroKuz <akuzcontact@gmail.com>
2025-06-04 13:23:01 +0200

    feat: add draw_move method

--- cell.py
+++ cell.py
@@ -1,4 +1,3 @@
-from tkinter import Canvas
 from graphics import Window, Line, Point
 
 
@@ -15,6 +14,9 @@ class Cell:
         self.__win = window
 
     def draw(self, start_point: Point, end_point: Point, fill_color: str = "black") -> None:
+        if self.__win is None:
+            return
+
         self.__x1 = start_point.x
         self.__x2 = end_point.x
         self.__y1 = start_point.y
@@ -40,3 +42,16 @@ class Cell:
             point2 = Point(self.__x2, self.__y2)
             self.__win.draw_line(Line(point1, point2), fill_color=fill_color)
 
+    def center_point(self):
+        x_center = abs(self.__x1 + self.__x2) // 2
+        y_center = abs(self.__y1 + self.__y2) // 2
+        return Point(x_center, y_center)
+
+    def draw_move(self, to_cell: "Cell", undo: bool = False) -> None:
+        fill_color = "red" if not undo else "grey"
+
+        current_cell_center_point = self.center_point()
+        to_cell_center_point = to_cell.center_point()
+
+        line = Line(current_cell_center_point, to_cell_center_point)
+        self.__win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -4,28 +4,20 @@ from cell import Cell
 
 def main():
     window = Window(800, 600)
-    cell = Cell(window)
-    point1 = Point(100, 100)
-    point2 = Point(200, 200)
-    cell.draw(point1, point2, fill_color="coral")
 
+    # Forward move
+    from_cell = Cell(window)
+    to_cell = Cell(window)
+    from_cell.draw(Point(400, 400), Point(500, 500))
+    to_cell.draw(Point(500, 400), Point(600, 500))
+    from_cell.draw_move(to_cell)
 
-    c = Cell(window)
-    c.has_left_wall = False
-    c.draw(Point(50, 50), Point(100, 100))
-
-    c = Cell(window)
-    c.has_right_wall = False
-    c.draw(Point(125, 125), Point(200, 200))
-
-    c = Cell(window)
-    c.has_bottom_wall = False
-    c.draw(Point(225, 225), Point(250, 250))
-
-    c = Cell(window)
-    c.has_top_wall = False
-    c.draw(Point(300, 300), Point(500, 500))
-
+    # Undo move
+    from_cell = Cell(window)
+    to_cell = Cell(window)
+    from_cell.draw(Point(200, 200), Point(300, 300))
+    to_cell.draw(Point(300, 200), Point(400, 300))
+    from_cell.draw_move(to_cell, undo=True)
 
     window.wait_for_close()
 
