635aeaa76af974673749e9988fe945b7219d9126
AlessandroKuz <akuzcontact@gmail.com>
2025-06-07 21:54:45 +0200

    test: add basic tests for maze creation

--- cell.py
+++ cell.py
@@ -2,19 +2,19 @@ from graphics import Window, Line, Point
 
 
 class Cell:
-    def __init__(self, window: Window) -> None:
-        self.has_left_wall = True
-        self.has_right_wall = True
-        self.has_top_wall = True
-        self.has_bottom_wall = True
+    def __init__(self, window: Window = None) -> None:
+        self.__has_left_wall = True
+        self.__has_right_wall = True
+        self.__has_top_wall = True
+        self.__has_bottom_wall = True
         self.__x1 = -1
         self.__x2 = -1
         self.__y1 = -1
         self.__y2 = -1
-        self.__win = window
+        self.__window = window
 
     def draw(self, start_point: Point, end_point: Point, fill_color: str = "black") -> None:
-        if self.__win is None:
+        if self.__window is None:
             return
 
         self.__x1 = start_point.x
@@ -22,25 +22,25 @@ class Cell:
         self.__y1 = start_point.y
         self.__y2 = end_point.y
 
-        if self.has_left_wall:
+        if self.__has_left_wall:
             point1 = Point(self.__x1, self.__y1)
             point2 = Point(self.__x1, self.__y2)
-            self.__win.draw_line(Line(point1, point2), fill_color=fill_color)
+            self.__window.draw_line(Line(point1, point2), fill_color=fill_color)
 
-        if self.has_right_wall:
+        if self.__has_right_wall:
             point1 = Point(self.__x2, self.__y1)
             point2 = Point(self.__x2, self.__y2)
-            self.__win.draw_line(Line(point1, point2), fill_color=fill_color)
+            self.__window.draw_line(Line(point1, point2), fill_color=fill_color)
 
-        if self.has_top_wall:
+        if self.__has_top_wall:
             point1 = Point(self.__x1, self.__y1)
             point2 = Point(self.__x2, self.__y1)
-            self.__win.draw_line(Line(point1, point2), fill_color=fill_color)
+            self.__window.draw_line(Line(point1, point2), fill_color=fill_color)
 
-        if self.has_bottom_wall:
+        if self.__has_bottom_wall:
             point1 = Point(self.__x1, self.__y2)
             point2 = Point(self.__x2, self.__y2)
-            self.__win.draw_line(Line(point1, point2), fill_color=fill_color)
+            self.__window.draw_line(Line(point1, point2), fill_color=fill_color)
 
     def center_point(self):
         x_center = abs(self.__x1 + self.__x2) // 2
@@ -54,4 +54,4 @@ class Cell:
         to_cell_center_point = to_cell.center_point()
 
         line = Line(current_cell_center_point, to_cell_center_point)
-        self.__win.draw_line(line, fill_color)
+        self.__window.draw_line(line, fill_color)
--- maze.py
+++ maze.py
@@ -12,41 +12,41 @@ class Maze:
             num_cols: int,
             cell_size_x: int,
             cell_size_y: int,
-            window: Window
+            window: Window = None
         ) -> None:
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.window = window
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__window = window
         self.__cells = []
         self.__create_cells()
 
     def __create_cells(self) -> None:
-        for col in range(self.num_cols):
+        for col in range(self.__num_cols):
             col_cells = []
-            for row in range(self.num_rows):
-                col_cells.append(Cell(self.window))
+            for row in range(self.__num_rows):
+                col_cells.append(Cell(self.__window))
             self.__cells.append(col_cells)
 
-        for col in range(self.num_cols):
-            for row in range(self.num_rows):
+        for col in range(self.__num_cols):
+            for row in range(self.__num_rows):
                 self.__draw_cell(col, row)
 
     def __draw_cell(self, column: int, row: int) -> None:
-        start_x = self.x1 + (self.cell_size_x * column)
-        start_y = self.y1 + (self.cell_size_y * row)
+        start_x = self.__x1 + (self.__cell_size_x * column)
+        start_y = self.__y1 + (self.__cell_size_y * row)
         start_point = Point(start_x, start_y)
-        end_x = self.x1 + (self.cell_size_x * (column + 1))
-        end_y = self.y1 + (self.cell_size_y * (row + 1))
+        end_x = self.__x1 + (self.__cell_size_x * (column + 1))
+        end_y = self.__y1 + (self.__cell_size_y * (row + 1))
         end_point = Point(end_x, end_y)
         self.__cells[column][row].draw(start_point, end_point)
         self.__animate()
 
     def __animate(self) -> None:
-        if self.window is None:
+        if self.__window is None:
             return
-        self.window.redraw()
+        self.__window.redraw()
         sleep(0.05)
